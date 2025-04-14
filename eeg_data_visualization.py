1fbe84fbd2bbfbf96dbf98669f6423a58fb2c185
Griffin <griffinbkeeler@gmail.com>
2025-04-14 17:53:49 -0500

    Added plotting function using matplotlib

---
+++ eeg_data_visualization.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot_eeg_csv(filepath= '/Users/griffinkeeler/PycharmProjects'
+                           '/neuro-blink-refined/raw_eeg_data.csv'):
+    df = pd.read_csv(filepath)
+
+    timestamps = df['timestamp']
+    channels = ['TP9', 'AF7', 'AF8', 'TP10']
+
+    plt.figure(figsize=(12, 6))
+    for ch in channels:
+        plt.plot(timestamps, df[ch], label=ch)
+
+    plt.title("EEG Signal Over Time")
+    plt.xlabel("Timestamp (s)")
+    plt.ylabel("Voltage (uV)")
+    plt.legend()
+    plt.show()
+
+def main():
+    plot_eeg_csv()
+
+if __name__ == "__main__":
+    main()
