fdb92d0c659f52144ab98c0739f201c5c73a60e8
Griffin <griffinbkeeler@gmail.com>
2025-04-14 18:24:42 -0500

    Filters raw data stream CSV file

--- signal_processing.py
+++ signal_processing.py
@@ -1,10 +1,17 @@
 # Used for bandpass filtering.
 from scipy.signal import butter, filtfilt
+import pandas as pd
 
-def bandpass_filter(data, lowcut=1.0, highcut=10.0, fs=256.0, order=4):
+
+def bandpass_filter(input_path='/Users/griffinkeeler/PycharmProjects'
+                     '/neuro-blink-refined/raw_eeg_data.csv',
+                    output_path='filtered_eeg_data.csv',
+                    lowcut=1.0, highcut=10.0, fs=256.0, order=4):
     """Applies a bandpass filter to the given channel. This filters the
      frequencies (hZ) into a specified range. The standard for low frequency
     filters is 1 Hz, and the standard for high frequency filters is 70 Hz."""
+
+    df = pd.read_csv(input_path)
 
     # Nyquist frequency - half the sampling rate.
     nyquist = 0.5 * fs
@@ -18,5 +25,18 @@ def bandpass_filter(data, lowcut=1.0, highcut=10.0, fs=256.0, order=4):
     # Designs the bandpass filter.
     b, a = butter(order, [low, high], btype='band')
 
-    # Applies filter to the EEG signal twice to avoid phase delay.
-    return filtfilt(b, a, data)
+    channels = ['TP9', 'AF7', 'AF8', 'TP10']
+
+    for ch in channels:
+        df[ch] = filtfilt(b, a, df[ch])
+
+    # Save filtered data to csv
+    df.to_csv(output_path, index=False)
+
+def main():
+    bandpass_filter()
+
+
+if __name__ == "__main__":
+    main()
+
