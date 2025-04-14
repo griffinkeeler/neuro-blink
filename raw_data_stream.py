7bd902644e48e500b1bbdaf0db5bde1199ccd6ad
Griffin <griffinbkeeler@gmail.com>
2025-04-14 17:28:49 -0500

    Increased data collection time for calibration

--- raw_data_stream.py
+++ raw_data_stream.py
@@ -5,44 +5,45 @@ from time import sleep
 # For creating a CSV file.
 import pandas as pd
 
-print("Looking for EEG streams...")
 
-# Gives the stream time to initialize.
-sleep(5)
+def eeg_data_to_csv():
+    # Finds all active LSL (Lab Streaming Layer) streams.
+    streams = resolve_streams()
 
-# Finds all active LSL (Lab Streaming Layer) streams.
-streams = resolve_streams()
+    # Loops through each stream 's' in 'streams'.
+    # if s.type() is EEG, 'next' gets the first match.
+    eeg_streams = next(s for s in streams if s.type() == 'EEG')
 
-# Loops through each stream 's' in 'streams'.
-# if s.type() is EEG, 'next' gets the first match.
-eeg_streams = next(s for s in streams if s.type() == 'EEG')
+    # Connects the code to the EEG data stream.
+    inlet = StreamInlet(eeg_streams)
 
-# Connects the code to the EEG data stream.
-inlet = StreamInlet(eeg_streams)
+    # Rows consisting of [timestamp, TP9, AF7, AF8, TP10
+    eeg_data = []
 
-print("Streaming EEG data...")
+    # Main data collection loop.
+    # The underscore indicates repetition in for loop.
+    for _ in range(20352):
+        # Pulls sample and timestamp from the EEG data stream.
+        sample, timestamp = inlet.pull_sample()
 
-# Rows consisting of [timestamp, TP9, AF7, AF8, TP10
-eeg_data = []
+        # Saves each row.
+        eeg_data.append([timestamp, sample[0], sample[1],
+                         sample[2], sample[3]])
 
-# Main data collection loop.
-# The underscore indicates repetition in for loop.
-for _ in range(750):
-    # Pulls sample and timestamp from the EEG data stream.
-    sample, timestamp = inlet.pull_sample()
+        # Optional delay for data collection.
+        sleep(0)
 
-    # Saves each row.
-    eeg_data.append([timestamp, sample[0], sample[1],
-                     sample[2], sample[3]])
+    # Creates a pandas DataFrame
+    df = pd.DataFrame(eeg_data, columns=['timestamp', 'TP9', 'AF7',
+                                         'AF8', 'AF9'])
 
-    # Optional delay for data collection.
-    sleep(0)
+    # Saves the DataFrame to a CSV file format
+    df.to_csv('raw_eeg_data.csv', index=False)
 
-# Creates a pandas DataFrame
-df = pd.DataFrame(eeg_data, columns=['timestamp', 'TP9', 'AF7',
-                                     'AF8', 'AF9'])
+    print("EEG data saved to raw_eeg_data.csv")
 
-# Saves the DataFrame to a CSV file format
-df.to_csv('raw_eeg_data.csv', index=False)
+def main():
+    eeg_data_to_csv()
 
-print("EEG data saved to raw_eeg_data.csv")
+if __name__ == "__main__":
+    main()
